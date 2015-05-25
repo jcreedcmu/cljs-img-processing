920a201e80feed35da2b39a3fa2b9e18c47290e8
Jason Reed <jcreed@gmail.com>
2015-05-25 16:19:51 -0400

    more refactoring

--- build/map.js
+++ build/map.js
@@ -1,43 +1,25 @@
-var fs = require('fs'),
-    PNG = require('node-png').PNG;
 var _ = require('underscore');
 var u = require('./util');
 
-function go() {
+u.with_image(__dirname + '/../resources/public/map.png', function(imdat, d) {
+  var newdat = d.createImageData(imdat.width, imdat.height);
+  for (var y = 0; y < imdat.height; y++) {
+    for (var x = 0; x < imdat.width; x++) {
+      var here = u.get(imdat, x, y);
+      var others = [u.get(imdat, x-1, y), u.get(imdat, x, y-1),
+		    u.get(imdat, x, y+1), u.get(imdat, x+1, y)]
 
-
-  var im = this;
-
-  var Canvas = require('canvas')
-  c = new Canvas(im.width,im.height);
-  d = c.getContext('2d');
-  d.fillStyle = "#def";
-  var newd = d.createImageData(im.width, im.height);
-
-  for (var y = 0; y < this.height; y++) {
-    for (var x = 0; x < this.width; x++) {
-      var here = u.get(im, x, y);
-      var others = [u.get(im, x-1, y), u.get(im, x, y-1), u.get(im, x, y+1), u.get(im, x+1, y)]
-
-      u.set3(newd, x, y, (here > others[0] || here > others[1] || here > others[2] || here > others[3])  ?
-	   [0,0,0] :
-	   (u.get(im, x, y) == 0x4b ?
-	    [128, 192, 192] :
-	    [255, 255, 255])  );
+      u.set3(newdat, x, y, (here > others[0] || here > others[1] || here > others[2] || here > others[3])  ?
+	     [0,0,0] :
+	     (u.get(imdat, x, y) == 0x4b ?
+	      [128, 192, 192] :
+	      [255, 255, 255])  );
     }
   }
 
 
-  d.putImageData(newd, 0, 0);
-
-  c.pngStream().pipe(fs.createWriteStream(__dirname + '/map-out.png'));
-
-  //  out.pack().pipe(fs.createWriteStream('out.png'));
-}
-
-fs.createReadStream('../resources/public/map.png')
-    .pipe(new PNG({ filterType: 4 }))
-    .on('parsed', go);
+  u.output_image(__dirname + '/map-out.png', d, newdat);
+});
 
 function optimizePts(pts) {
   var newpts = [];
--- build/util.js
+++ build/util.js
@@ -1,17 +1,19 @@
+var fs = require('fs');
+var PNG = require('node-png').PNG;
+var Canvas = require('canvas');
+
 function get(im, x, y) {
   var g = get3(im, x, y);
   return g[1];
 }
 
 function get3(im, x, y) {
-   if (x < 0 || x >= im.width || y < 0 || y >= im.height) return [255,255,255];
-
+  if (x < 0 || x >= im.width || y < 0 || y >= im.height) return [255,255,255];
   var ix = (im.width * y + x) << 2;
   return [im.data[ix],
 	  im.data[ix + 1],
 	  im.data[ix + 2]];
 }
-
 
 function set3(im, x, y, c) {
   var base = (im.width * y + x) << 2;
@@ -21,6 +23,24 @@ function set3(im, x, y, c) {
   im.data[base + 3] = 255;
 }
 
+function with_image(filename, k) {
+  fs.createReadStream(filename)
+    .pipe(new PNG({ filterType: 4 }))
+    .on('parsed', function () {
+      var imdat = this;
+      c = new Canvas(imdat.width,imdat.height);
+      d = c.getContext('2d');
+      k(imdat, d);
+    });
+}
+
+function output_image(filename, d, imdat) {
+  d.putImageData(imdat, 0, 0);
+  c.pngStream().pipe(fs.createWriteStream(filename));
+}
+
 exports.get = get;
 exports.get3 = get3;
 exports.set3 = set3;
+exports.with_image = with_image;
+exports.output_image = output_image;
