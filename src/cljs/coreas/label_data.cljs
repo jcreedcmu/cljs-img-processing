908f437530d0f3c8768db659beace769eea4ade0
Jason Reed <jcreed@gmail.com>
2015-06-13 16:23:24 -0400

    minimal labels

---
+++ src/cljs/coreas/label_data.cljs
@@ -0,0 +1,55 @@
+(ns coreas.label-data)
+
+(def pos->label
+  [{:pos [64 393], :text "piada"}
+   {:pos [113 414], :text "koennif"}
+   {:pos [105 444], :text "yol"}
+   {:pos [179 437], :text "ezdi"}
+   {:pos [387 335], :text "naga sugira"}
+   {:pos [163 343], :text "nivdal"}
+   {:pos [262 275], :text "shiao"}
+   {:pos [126 197], :text "sikau"}
+   {:pos [230 201], :text "shiaovets"}
+   {:pos [238 121], :text "boshionai"}
+   {:pos [342 110], :text "bissholmi"}
+   {:pos [383 254], :text "mi yon"}
+   {:pos [458 145], :text "stolups"}
+   {:pos [621 250], :text "gurakand"}
+   {:pos [883 531], :text "zoxiai"}
+   {:pos [819 388], :text "zufkand"}
+   {:pos [779 350], :text "ziem"}
+   {:pos [767 414], :text "qunmi"}
+   {:pos [698 470], :text "ashigam"}
+   {:pos [700 373], :text "nyvev"}
+   {:pos [575 373], :text "vlori"}
+   {:pos [606 438], :text "munishtaq vu"}
+   {:pos [544 490], :text "jdayalur"}
+   {:pos [598 520], :text "negima"}
+   {:pos [567 610], :text "rujokatch"}
+   {:pos [541 564], :text "jatmiyan"}
+   {:pos [423 444], :text "wolaraum"}
+   {:pos [347 501], :text "gorvied"}
+   {:pos [242 489], :text "zbegyanda"}
+   {:pos [200 562], :text "nartu"}
+   {:pos [200 662], :text "gorgobya"}
+   {:pos [198 625], :text "noboro"}
+   {:pos [453 578], :text "iwoalye"}
+   {:pos [282 601], :text "syeg ims"}
+   {:pos [378 561], :text "mayarapit"}
+   {:pos [457 658], :text "huatosurak"}
+   {:pos [385 675], :text "huiyepitnaya"}
+   {:pos [384 747], :text "tlorukpit"}
+   {:pos [492 697], :text "kunzi"}
+   {:pos [555 671], :text "urrakeny"}
+   {:pos [566 735], :text "juta"}
+   {:pos [640 691], :text "narik"}
+   {:pos [689 645], :text "unbara"}
+   {:pos [705 685], :text "pengerra"}
+   {:pos [834 738], :text "oskeny"}
+   {:pos [776 730], :text "daloe"}
+   {:pos [715 734], :text "gorik"}
+   {:pos [738 631], :text "nonggadum"}
+   {:pos [776 649], :text "ribbonnad"}
+   {:pos [809 678], :text "minar"}
+   {:pos [877 703], :text "cubre"}
+   {:pos [902 768], :text "tarodaro"}])
